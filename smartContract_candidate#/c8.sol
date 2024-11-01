f22a957e5904de6cfcddb1fcbbb31720162cc10e
Hyejin Lee <hyejinclairelee@gmail.com>
2024-11-02 01:25:41 +0900

    Update c8.sol

--- smartContract_candidate#/c8.sol
+++ smartContract_candidate#/c8.sol
@@ -627,12 +627,13 @@ contract Verifier {
 
     function verifyTx(Proof memory proof, uint[11] memory input) public returns (bool r) {
         uint[] memory inputValues = new uint[](11);
-        //check if nullifier has been used before
-        require(nulTable[inputValues[9]] == false);
-
+    
         for(uint i = 0; i < input.length; i++){
             inputValues[i] = input[i];
         }
+        //check if nullifier has been used before
+        require(nulTab[inputValues[14]] == false);
+        require(inputValues[15] == 1);
         if (verify(inputValues, proof) == 0) {
             //if proof passed, nullifer = true to prevent this prover from re-evaluating.
             nulTable[inputValues[9]] = true;
