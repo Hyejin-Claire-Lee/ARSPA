5220ade14e65ccb65f5461bb770f1b2a4d43581e
Hyejin Lee <hyejinclairelee@gmail.com>
2024-11-02 01:27:22 +0900

    Update c7.sol

--- smartContract_candidate#/c7.sol
+++ smartContract_candidate#/c7.sol
@@ -561,23 +561,25 @@ contract Verifier {
         Pairing.G1Point c;
     }
 
-    //nullifier table
-    mapping (uint256 => bool) public nulTable;
-
-    // eID=msg.sender는 cID, ratingToken, Ethereum address(as a key)를 가진 사람이다
-    struct eID {
-        uint[7] cID;
-        bool ratingToken;
+    //=====================DATA DECLARATION============================
+    //eID struct. Each pseudonym is associated with an eID.
+    struct eID {        //Evaluator identifier.
+    uint[13] cID;       //cID stores up to 13 candidate identifiers, with unused entries set to 0.
+    bool ratingToken;   //The rating token is the evaluator's right to review.
     }
-    mapping (address => eID) public table;
+    
+    //List of evaluators.
+    mapping (address => eID) internal table;
 
-    //transaction들 저장할 table3
-    uint[7][] public allReviews;
-    // mapping (address => uint256[][]) allReviews;
+    uint[13][] public allReviews;    // mapping (address => uint256[][]) allReviews;
     uint public weight = 0;
 
-    //Event
-    event GetAllReview(uint256[7][]);
+    //Table of nullifiers
+    mapping(uint256 => bool) internal nulTab;
+
+    //Get all reviews recorded on the blockchain. Consider using IPFS on a large scale.
+    event GetAllReview(uint256[13][]);
+    //=================================================================~~
 
 
     function verifyingKey() pure internal returns (VerifyingKey memory vk) {
